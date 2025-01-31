3b2387fd502f84536a1b740b11e885f94d634a42
Rajas Yellapurkar <rajasyellapurkargit@gmail.com>
2025-01-31 08:31:20 +0530

    Sorting I assignments

---
+++ R7.DSA/Sorting1/MergeSortedArray.cs
@@ -0,0 +1,45 @@
+﻿namespace R7.DSA.Sorting1
+{
+    internal class MergeSortedArray
+    {
+        // https://leetcode.com/problems/merge-sorted-array/description/
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int p1 = 0;
+            int p2 = 0;
+            int i = 0;
+            int[] result = new int[m + n];
+            while (p1 < m && p2 < n)
+            {
+                if (nums2[p2] < nums1[p1])
+                {
+                    result[i] = nums2[p2];
+                    p2++;
+                    i++;
+                }
+                else
+                {
+                    result[i] = nums1[p1];
+                    p1++;
+                    i++;
+                }
+            }
+            while (p2 < n)
+            {
+                result[i] = nums2[p2];
+                p2++;
+                i++;
+            }
+            while (p1 < m)
+            {
+                result[i] = nums1[p1];
+                p1++;
+                i++;
+            }
+            for (int j = 0; j < (m + n); j++)
+            {
+                nums1[j] = result[j];
+            }
+        }
+    }
+}
---
+++ R7.DSA/Sorting1/ShortestUnsortedContinousSubArray.cs
@@ -0,0 +1,49 @@
+﻿namespace R7.DSA.Sorting1
+{
+    internal class ShortestUnsortedContinousSubArray
+    {
+        //https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+        public int FindUnsortedSubarray(int[] nums)
+        {
+            int n = nums.Length;
+            int[] sortedArr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                sortedArr[i] = nums[i];
+            }
+            Array.Sort(sortedArr);
+            int p1 = 0;
+            int p2 = n - 1;
+            while (p1 < n)
+            {
+                if (sortedArr[p1] == nums[p1])
+                {
+                    p1++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (p1 == n)
+            {
+                return 0;
+            }
+            else
+            {
+                while (p2 >= 0)
+                {
+                    if (sortedArr[p2] == nums[p2])
+                    {
+                        p2--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return p2 - p1 + 1;
+        }
+    }
+}
